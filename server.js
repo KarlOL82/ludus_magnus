a6b521b1a994a70d298df56e5197cf953ef3497e
KarLOL82 <klinfeldt@gmail.com>
2022-09-10 12:55:24 -0700

    server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+// const sequelize = require('./config/connection');
+const utils = require("util");
+const mysql = require("mysql2");
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'N3buchadnezzar!3',
+      database: 'ludus_db'
+    },
+    console.log(`Connected to the teamDirectory_db database.`)
+);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
