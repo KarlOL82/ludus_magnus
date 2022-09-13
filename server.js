5cc1356cc3fab1f980d334c9fbc387fcff258569
KarLOL82 <klinfeldt@gmail.com>
2022-09-12 21:54:32 -0700

    fixed server.js

--- server.js
+++ server.js
@@ -1,37 +1,33 @@
 const path = require('path');
 const express = require('express');
-const handlebars = require('express-handlebars');
+const exhbs = require('express-handlebars');
 const session = require('express-session');
-// const sequelize = require('./config/connection');
+
 const utils = require("util");
 const mysql = require("mysql2");
 
 
 const app = express();
-// const sequelize = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 
+const handlebars = exhbs.create(); 
+app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-app.engine('handlebars', handlebars({
-    layoutsDir: __dirname + '/views/layouts',
-    }));
+
 
     
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.static('public'));
-app.get('/', (req, res) => res.send('Hello World !'));
+// app.get('/', (req, res) => res.send('Hello World !'));
 
 app.get('/', (req, res) => {
     //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-    res.render('homepage', {layout : 'main'});
+    res.render('main', {layout : 'homepage'});
     });
-
 
 
 app.listen(PORT, () => {
