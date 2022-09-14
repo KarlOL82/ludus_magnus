86aeaf0e4a06072a9b25197988cea9fc0ca35e64
B.O.A Addington <Franswarduvar>
2022-09-13 18:18:38 -0700

    commiting new code so I can go and pull down main

--- public/js/profanity.js
+++ public/js/profanity.js
@@ -1,5 +1,36 @@
 var cleanser = require('profanity-cleanser');
- 
-cleanser.setLocale()
- 
 var output = cleanser.getDictionary();
+
+cleanser.setLocale()
+
+// var inputString = "Your ass is shit"
+// //cleanser.setLocale(['hi', 'es', 'ja']);
+
+// var output = cleanser.replace(inputString, 'word', 'FOOBAR');
+
+// console.log(output);
+
+
+
+
+
+// set locale to hindi
+filter.setLocale(['hi', 'en-uk']);
+var locale = filter.getLocale();
+
+// Input string with bad words... 
+var inputString = "Bhadve is a very bad word is Hindi. It means pimp in English. You should not use words like these. Some other examples are lund which means penis or randi which means whore. Bad language should never be used. It is hurtful and disrespectful. Live and let live!";
+
+var grawlix_output = filter.replace(inputString, 'grawlix');
+
+var default_star_output = filter.replace(inputString);
+
+var word_output = filter.replace(inputString, 'word', 'BADWORD');
+
+console.log("-------------- OUTPUTS ---------------");
+console.log("\nSet locale is " + locale.toString());
+console.log("\nOffensive input string is:\n" +  inputString);
+console.log("\nInput filtered with grawlix:\n" +  grawlix_output);
+console.log("\nInput filtered with default star output:\n" +  default_star_output);
+console.log("\nInput filtered with word output:\n" +  word_output);
+console.log(word_output);
