578ec98e141f61d8770334244f3b69b97fd4ce3a
KarLOL82 <klinfeldt@gmail.com>
2022-09-13 18:09:54 -0700

    working on homepage branch

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const express = require('express');
-const handlebars = require('express-handlebars');
+const exhbs = require('express-handlebars');
 const session = require('express-session');
-// const sequelize = require('./config/connection');
+
 const utils = require("util");
 const mysql = require("mysql2");
 
@@ -10,35 +10,25 @@ const mysql = require("mysql2");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
+const handlebars = exhbs.create(); 
 app.engine('handlebars', handlebars.engine);
-
 app.set('view engine', 'handlebars');
 
 
+
+    
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.static('public'));
-app.get('/', (req, res) => res.send('Hello World !'));
+
 
 app.get('/', (req, res) => {
-    //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+    
     res.render('main', {layout : 'homepage'});
     });
 
-
-
-// const db = mysql.createConnection(
-//     {
-//       host: 'localhost',
-//       user: 'root',
-//       password: 'Delicon1234%',
-//       database: 'ludus_db'
-//     },
-//     console.log(`Connected to the teamDirectory_db database.`)
-// );
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
