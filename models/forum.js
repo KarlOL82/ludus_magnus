3f8a57426a0949b71da844d51ec26ee2b3061cf1
Matt Burns <matt.burns01@gmail.com>
2022-09-14 20:32:14 -0700

    forum model

---
+++ models/forum.js
@@ -0,0 +1,59 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Forum extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+Forum.init(
+  {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // username: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false,
+    // },
+    forumtitle: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len:[30],
+      },
+    },
+    gamename: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [30],
+      },
+    },
+    feedpost: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate:{
+            len: [500],
+        }   
+    }
+},
+  {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'forum',
+  }
+);
+
+module.exports = Forum;
