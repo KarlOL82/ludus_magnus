b18f2f62ca8984749b4f3ba0a41d08da3a295402
KarLOL82 <klinfeldt@gmail.com>
2022-09-15 21:23:17 -0700

    nope

---
+++ routes/homeRoutes.js
@@ -0,0 +1,79 @@
+const router = require('express').Router();
+const { User, Forum, GameChat } = require('../models');
+const { withAuth, withNoAuth } = require('../utils/auth');
+const { Op } = require('sequelize');
+
+// Route "/"
+
+// Route "/login"
+
+app.get('/', (req, res) => {
+    
+    res.render('homepage', {layout : 'main'});
+    });
+
+// TODO: Add a comment describing the functionality of the withAuth middleware
+router.get('/', withAuth, async (req, res) => {
+  try {
+
+    const where = {
+      user_id: {
+        [Op.ne]: req.session.user_id,
+      },
+    };
+
+    const { search_name } = req.query;
+
+    if(search_name) {
+      where.name = {
+        [Op.like]: `%${search_name}%`
+      };
+    }
+
+    const userData = await Forum.findAll({
+      where,
+      include: {
+        model: User,
+        attributes: ['id', 'forumtitle'],
+      },
+    });
+
+    const forums = userData.map((forum) => forum.get({ plain: true }));
+
+    res.render('homepage', {
+      foodItems,
+      searchName: search_name,
+      // TODO: Add a comment describing the functionality of this property
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/profilePage', withAuth, async (req, res) => {
+
+  const userData = await User.findByPk(req.session.user_id, {
+    attributes: ['id','user_name'],
+    include: Forum
+  });
+
+  const user = userData.toJSON();
+
+  console.log(user);
+
+  res.render('user', {
+    user,
+    logged_in: req.session.logged_in,
+  });
+});
+
+router.get('/login', withNoAuth, (req, res) => {
+  res.render('login');
+});
+
+router.get('/signUp', withNoAuth, (req, res) => {
+  res.render('signUp');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const path = require('path');
 const express = require('express');
 const exhbs = require('express-handlebars');
 const session = require('express-session');
-
+const routes = require('./routes');
 const utils = require("util");
 const mysql = require("mysql2");
-
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -22,14 +22,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.static('public'));
+app.use(routes);
 
-
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
     
-    res.render('homepage', {layout : 'main'});
-    });
+//     res.render('homepage', {layout : 'main'});
+//     });
 
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
+
+//   sequelize.sync({ force: false }).then(() => {
+//     app.listen(PORT, () =>
+//       console.log(`Now listening: http://localhost:${PORT}`)
+//     );
+//   });
