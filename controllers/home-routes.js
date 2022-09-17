15ac7820204a7cc39c4ab4f12abc61299860086e
KarLOL82 <klinfeldt@gmail.com>
2022-09-17 09:13:27 -0700

    having trouble with user route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -13,38 +13,25 @@ const { Op } = require('sequelize');
 //     });
 
 
-router.get('/home-routes', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-
-    const where = {
-      user_id: {
-        [Op.ne]: req.session.user_id,
-      },
-    };
-
-    const { search_name } = req.query;
-
-    if(search_name) {
-      where.name = {
-        [Op.like]: `%${search_name}%`
-      };
-    }
-
-    const userData = await Forum.findAll({
-      where,
-      include: {
-        model: User,
-        attributes: ['id', 'forumtitle'],
-      },
+    // Get all projects and JOIN with user data
+    const chatData = await GameChat.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
     });
 
-    const forums = userData.map((forum) => forum.get({ plain: true }));
+    // Serialize data so the template can read it
+    const chats = chatData.map((gameChat) => gameChat.get({ plain: true }));
 
-    res.render('homepage', {
-      forums,
-      searchName: search_name,
-      
-      logged_in: req.session.logged_in,
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      chats, 
+      logged_in: req.session.logged_in 
     });
   } catch (err) {
     res.status(500).json(err);
@@ -53,12 +40,12 @@ router.get('/home-routes', withAuth, async (req, res) => {
 
 router.get('/profilePage', withAuth, async (req, res) => {
 
-  const userData = await User.findByPk(req.session.user_id, {
+  const dbUserData = await User.findByPk(req.session.user_id, {
     attributes: ['id','user_name'],
     include: Forum
   });
 
-  const user = userData.toJSON();
+  const user = dbUserData.toJSON();
 
   console.log(user);
 
--- db/schema.sql
+++ db/schema.sql
@@ -2,5 +2,5 @@ DROP DATABASE IF EXISTS user_db;
 
 CREATE DATABASE user_db;
 
-USE user_db;
+
 
